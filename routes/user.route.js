7711c85d409e8d33d8a0e6bd8f61caa56893e53b
unknown <josuenojaguilar@gmail.com>
2020-02-11 14:07:56 -0600

    ZooV2User

---
+++ routes/user.route.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var express = require('express');
+var userController = require('../controllers/user.controller');
+var api = express();
+var middlewareAuth = require('../middlewares/authenticated');
+var connectMultiparty = require('connect-multiparty');
+var middlewareUpload = connectMultiparty({ uploadDir: './uploads/users'});
+
+api.post('/saveUser', userController.saveUser);
+api.post('/login', userController.login);
+api.get('/pruebaMiddleware',middlewareAuth.ensureAuth, userController.pruebaMiddleware);
+api.put('/updateUser/:id', middlewareAuth.ensureAuth, userController.updateUser);
+api.post('/uploadImage/:id', [middlewareAuth.ensureAuth, middlewareUpload], userController.uploadImage);
+api.get('/getImage/:id/:image',middlewareAuth.ensureAuth, userController.getImage);
+
+//Rutas a las cuales solo puede acceder un role admin
+api.get('/listUsers', middlewareAuth.ensureAuthAdmin, userController.listUsers);
+
+module.exports = api;
